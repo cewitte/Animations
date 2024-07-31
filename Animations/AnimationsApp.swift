ae8286a2006bcf11d22aae49fecf7419f71708ed
Carlos Eduardo Witte <cewitte@icloud.com>
2024-07-31 08:12:47 -0300

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Carlos Eduardo Witte on 31/07/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
